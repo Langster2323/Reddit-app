c7de5c81b5929a96c0412306a648e68dd41d878e
Ahkeem Lang <langahkeem@gmail.com>
2016-07-27 15:37:10 -0500

    Finished creating CRUD actions for links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,14 +1,14 @@
 class LinksController < ApplicationController
   def index
-    render locals: {
-      links: Link.all?
-    }
+    render template: 'link/index.html.erb', locals: { link: Link.all }
   end
 
   def show
-    render locals: {
-      link: Link.find(params[:id])
-    }
+    if Link.exists?(params[:id])
+      render template: 'link/show.html.erb', locals: { link: Link.find(params[:id])}
+    else
+      render html: "Entity Not Found.", status: 404
+    end
   end
 
   def new
@@ -24,10 +24,28 @@ class LinksController < ApplicationController
   end
 
   def create
-    render
+    link = Link.new
+    link.title = params[:link][:title]
+    if link.save
+      redirect_to_link_path(link)
+    else
+      flash[:alert] = "Link could not be saved"
+      render template: 'link/new.html.erb', locals: {
+        link: link
+      }
+    end
   end
 
   def update
+    link = Link.find(params[:id])
+    link.title = params[:link][:title] if params[:link][:title].present?
+    if link.save
+      redirect_to_link_path(link)
+    else
+      render template: '/link/new.html.erb', locals: {
+        link: link
+      }
+    end
   end
 
   def destroy
