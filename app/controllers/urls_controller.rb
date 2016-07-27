33c54f480f2c0e46d2b05b72ecac41eddc93db6b
Ahkeem Lang <langahkeem@gmail.com>
2016-07-27 15:53:26 -0500

    Finished crud actiond for url controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,34 @@
 class UrlsController < ApplicationController
+  def index
+    render template: 'url/index.html.erb', locals: {
+      Urls: Url.all,
+    }
+  end
+
+  def show
+    if Url.exists?(params[:id])
+    Url.find(params[:id])
+      render template: 'url/show.html.erb'
+    else
+      render html: "Url Not Found", status: 404
+    end
+  end
+
+  def new
+    render locals: {
+      url: Url.new
+    }
+  end
+
+  def edit
+    render locals: {
+      url: Url.find(params[:id])
+    }
+  end
+
+  def create
+  end
+
+  def update
+  end
 end
