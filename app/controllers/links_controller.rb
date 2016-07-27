6679da6297fb8ccd953dcdad71e980f4bdcf84ae
Ahkeem Lang <langahkeem@gmail.com>
2016-07-27 15:15:01 -0500

    Working on my CRUD for links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,36 @@
 class LinksController < ApplicationController
+  def index
+    render locals: {
+      links: Link.all?
+    }
+  end
+
+  def show
+    render locals: {
+      link: Link.find(params[:id])
+    }
+  end
+
+  def new
+    render locals: {
+      link: Link.new
+    }
+  end
+
+  def edit
+    render locals: {
+      link: Link.find(params[:id])
+    }
+  end
+
+  def create
+    render
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
