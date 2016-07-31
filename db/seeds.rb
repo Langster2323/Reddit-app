ce3b79a98357db311bed10c5aedad7313c5c5582
Ahkeem Lang <langahkeem@gmail.com>
2016-07-31 13:18:39 -0500

    Testing my seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do
+20.times do |n|
   link_title = [
     Faker::Company.catch_phrase,
     ["OG of", "ready to", "flew by", "discovered by"].sample,
     Faker::Hipster.word,
   ].join(" ")
-  link = link.create(title: link_title,
+  link = Link.create(title: link_title,
           url: Faker::Hacker.adjective+Faker::Hacker.noun+".com")
           Vote.create(link_id: link.id)
 end
