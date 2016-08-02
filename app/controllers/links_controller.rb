abe649d45e80efd46b76831468b40f856dcee51e
Ahkeem Lang <langahkeem@gmail.com>
2016-08-02 15:16:02 -0500

    Resetted my database, created instance variables for link in link controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,58 +1,86 @@
 class LinksController < ApplicationController
   def index
-    render template: 'links/index.html.erb', locals: { link: Link.all }
+    @links = Link.includes(:votes, :urls).limit(10)
+    @link = Link.new
+    # render template: 'links/index.html.erb', locals: { link: Link.all }
   end
 
   def show
-    if Link.exists?(params[:id])
-      render template: 'links/show.html.erb', locals: { link: Link.find(params[:id])}
-    else
-      render html: "Entity Not Found.", status: 404
-    end
+    # if Link.exists?(params[:id])
+    #   render template: 'links/show.html.erb', locals: { link: Link.find(params[:id])}
+    # else
+    #   render html: "Entity Not Found.", status: 404
+    # end
   end
 
   def new
-    render locals: {
-      link: Link.new
-    }
+    @link = Link.new
+  #   render locals: {
+  #     link: Link.new
+  #   }
   end
 
   def edit
-    render locals: {
-      link: Link.find(params[:id])
-    }
+    # render locals: {
+    #   link: Link.find(params[:id])
+    # }
   end
 
   def create
-    link = Link.new
-    link.title = params[:link][:title]
-    link.url = params[:link][:url]
+    @link = Link.new(link_params)
 
-    if link
-      link.save
-      redirect_to action: "index"
+    if @link.save
+      redirect_to @link, notice: "Link was successfully created"
     else
-      flash[:alert] = "Link could not be saved"
-      render template: 'link/new.html.erb', locals: {
-        link: link
-      }
+      render :new
     end
+    # link = Link.new
+    # link.title = params[:link][:title]
+    # link.url = params[:link][:url]
+    #
+    # if link
+    #   link.save
+    #   redirect_to action: "index"
+    # else
+    #   flash[:alert] = "Link could not be saved"
+    #   render template: 'link/new.html.erb', locals: {
+    #     link: link
+    #   }
+    # end
   end
 
   def update
-    link = Link.find(params[:id])
-    link.title = params[:title] if params[:title].present?
-    link.vote = params[:vote] if params[:vote].present?
-    if link.save
-      redirect_to link_path(link)
+    if @link.update(link_params)
+      redirect_to @link, notice: "Link was successfully update"
     else
-      render template: '/link/new.html.erb', locals: {
-        link: link
-      }
+      render :edit
     end
+
+    # link = Link.find(params[:id])
+    # link.title = params[:title] if params[:title].present?
+    # link.vote = params[:vote] if params[:vote].present?
+    # if link.save
+    #   redirect_to link_path(link)
+    # else
+    #   render template: '/link/new.html.erb', locals: {
+    #     link: link
+    #   }
+    # end
   end
 
   def destroy
+    @link.destroy
+    redirect_to link_url, notice: "Link was successfully destroyed"
   end
 
+
+  private
+
+  def set_link
+    @link = Link.find(params[:id])
+  end
+
+  def link_params
+    params.require(:link).permit(:vote, :url, :title)
+  end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,7 @@ class VotesController < ApplicationController
   def create
     vote = Vote.new
     vote.link_id = params[:vote][:link_id]
+    vote.save
     redirect_to :back
   end
 end
